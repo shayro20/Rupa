535a1e8f8c0e14eb4e286b18dc1e21acc969be34
שי רואימי <shay3156@gmail.com>
2023-11-11 18:44:26 +0200

    !

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Navbar from "./components/Navbar";
-import { dot } from "node:test/reporters";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
